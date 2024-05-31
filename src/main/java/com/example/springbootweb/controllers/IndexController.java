57d5483aaaf60c64a4b10fffdb39992f1729aa9c
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-30 23:27:04 -0500

    Creando un controlador y una vista

--- src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -1,5 +1,15 @@
 package com.example.springbootweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // marca la clase como tipo controlador
 public class IndexController {
-    // cada clase controladora debe terminar con controller en mayuscula
+
+    // el controlador se encarga de controlar las peticiones del usuario
+    @GetMapping(path = "/index")
+    public String index() {
+
+        return "index";
+    }
 }
