a1b58e4ca6dea2cdcab7a7a224be1d822a70eb31
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-31 21:34:26 -0500

    Pasar datos a la vista

--- src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // marca la clase como tipo controlador
@@ -8,7 +9,16 @@ public class IndexController {
 
     // el controlador se encarga de controlar las peticiones del usuario
     @GetMapping(path = "/index")
-    public String index() {
+    public String index(ModelMap model) {
+        // Con el Model model se puede enviar datos por medio de llave valor a la vista en este caso es el attribute con dos argumentos
+        // tiene la misma funcion
+        // Con el ModelMap es una clase que va a heredar de una clase map
+
+        //Tambien se puede usar Map y se indica los tipos de datos Map<String, object>map y tiene el metodo put
+
+        //ModelAndView con el metodo es addObject le podemos asignar el normbre de la vista a cargar
+
+        model.addAttribute("titulo","SpringWeb");
 
         return "index";
     }
