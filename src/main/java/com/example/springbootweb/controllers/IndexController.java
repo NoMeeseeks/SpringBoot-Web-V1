c8707c1c535ebea14494f2882ec04c3c15b208d0
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-06-01 21:02:53 -0500

    Request Param

--- src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -4,6 +4,7 @@ import com.example.springbootweb.model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -43,13 +44,23 @@ usuario.setEmail("Xavier@gmail.com");
 
     @RequestMapping(method = RequestMethod.GET, path = "/listar")
     public String listar(Model model){
+//        List<Usuario> usuarios = new ArrayList<>();
+//        usuarios.add(new Usuario("Xavier","Santamaria","xavi@gmail.com"));
+//        usuarios.add(new Usuario("Luis","Coro","xavi21313@gmail.com"));
+//        usuarios.add(new Usuario("Pepe","Cadena","xavi2121fds@gmail.com"));
+//        usuarios.add(new Usuario("Carlos","Francis","xavnfakldf0113i@gmail.com"));
+        model.addAttribute("titulo","Lista de usuarios");
+//        model.addAttribute("usuarios",usuarios);
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios(){
         List<Usuario> usuarios = new ArrayList<>();
         usuarios.add(new Usuario("Xavier","Santamaria","xavi@gmail.com"));
         usuarios.add(new Usuario("Luis","Coro","xavi21313@gmail.com"));
         usuarios.add(new Usuario("Pepe","Cadena","xavi2121fds@gmail.com"));
         usuarios.add(new Usuario("Carlos","Francis","xavnfakldf0113i@gmail.com"));
-        model.addAttribute("titulo","Lista de usuarios");
-        model.addAttribute("usuarios",usuarios);
-        return "listar";
+        return usuarios;
     }
 }
